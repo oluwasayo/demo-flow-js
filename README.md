a147b12f4c6e46078e39284c0a1ae34030ef4bd1
Sayo Oladeji <sayo@vaadin.com>
2018-03-13 13:37:31 +0200

    Demo for Flow's JavaScript annotation

---
+++ README.md
@@ -0,0 +1,2 @@
+Run using `mvn jetty:run` and open [http://localhost:8080](http://localhost:8080) in browser. 
+You should see an alert that shows that the JS file was loaded.
---
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -0,0 +1,9 @@
+package com.vaadin.starter.skeleton;
+
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+@JavaScript("src/stuff.js")
+public class MainView extends Div {  }
